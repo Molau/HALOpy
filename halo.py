f9424360373500240a69cfde4e47883944623cdf
Molau <sirko@molau.de>
2026-02-08 22:56:09 +0100

    Export Flask app for WSGI servers (uWSGI/Gunicorn)

--- halo.py
+++ halo.py
@@ -7,7 +7,13 @@ src_path = Path(__file__).parent / 'src'
 sys.path.insert(0, str(src_path))
 
 from halo.config import get_deployment_mode, is_cloud_mode
-from halo.web.app import main
+from halo.web.app import create_app, main
+
+# Create Flask app instance for WSGI servers (uWSGI, Gunicorn, etc.)
+app = create_app()
+
+# Alias for uWSGI default callable name
+application = app
 
 if __name__ == '__main__':
     # Detect deployment mode
