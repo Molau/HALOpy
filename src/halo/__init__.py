10245f62be873bf13ea67e004bdb976941a25645
Molau <sirko@molau.de>
2026-03-08 13:01:19 +0100

    Release 3.1.6: fix Windows dependency install for auto-update

--- src/halo/__init__.py
+++ src/halo/__init__.py
@@ -3,6 +3,6 @@ HALO - Atmospheric Halo Observation Software
 Python translation of the original Borland Pascal software
 """
 
-__version__ = "3.1.5"
+__version__ = "3.1.6"
 __version_date__ = "2026-03-08"
 __author__ = "Original: 1992, Python Translation: 2025"
